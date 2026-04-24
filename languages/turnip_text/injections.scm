4c23e7d35a71f4f7f368a999901ca048daf1fcdd
Samuel Stark <samuel.winslow.stark@gmail.com>
2026-04-25 00:22:38 +0100

    Fixup language injection for short Python

--- languages/turnip_text/injections.scm
+++ languages/turnip_text/injections.scm
@@ -1,3 +1,6 @@
 (eval_bracket
   (eval_bracket_internal) @injection.content
   (#set! injection.language "python"))
+(eval_bracket_short
+  (eval_bracket_short_internal) @injection.content
+  (#set! injection.language "python"))
