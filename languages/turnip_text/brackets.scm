64567a9d4ac881e84762864c5358bf0c3afcd3a6
Samuel Stark <samuel.winslow.stark@gmail.com>
2026-04-25 00:11:02 +0100

    Update & attempt to separate short and long eval-brackets

--- languages/turnip_text/brackets.scm
+++ languages/turnip_text/brackets.scm
@@ -1,11 +1,1 @@
-(eval_bracket
-    (eval_bracket_open) @open
-    (eval_bracket_close) @close
-    (#set! rainbow.exclude))
-; (eval1 "[-" @open "-]" @close (#set! rainbow.exclude))
-; (eval2 "[--" @open "--]" @close (#set! rainbow.exclude))
-; (eval3 "[---" @open "---]" @close (#set! rainbow.exclude))
 (scope "{" @open "}" @close)
-; (raw1 "#{" @open "}#" @close (#set! rainbow.exclude))
-; (raw2 "##{" @open "}##" @close (#set! rainbow.exclude))
-; (raw3 "###{" @open "}###" @close (#set! rainbow.exclude))
--- languages/turnip_text/highlights.scm
+++ languages/turnip_text/highlights.scm
@@ -2,11 +2,12 @@
 
 (comment) @comment
 
-(eval_bracket_open) @punctuation.bracket ; for [- etc.
-(eval_bracket_close) @punctuation.bracket ; for [- etc.
+(eval_bracket_open) @text.literal ; for [- etc.
+(eval_bracket_close) @text.literal ; for -] etc.
+(eval_bracket_short_open) @text.literal ; for [
+(eval_bracket_short_close) @text.literal ; for ]
 
 (dash) @punctuation.special ; for -- ---
-(escaped) @string.escape ; for -- ---
+(escaped) @string.escape ; for \x
 
-; TODO comment?
 ; normal braces { } are handled by rainbow
